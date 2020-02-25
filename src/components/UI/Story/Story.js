9239875253c24df382e57218d39dd82f1e90821c
Devon Deason <devon.d.deason@gmail.com>
2020-02-25 11:14:24 -0700

    separate out component for StoryTitleDomain

--- src/components/UI/Story/Story.js
+++ src/components/UI/Story/Story.js
@@ -2,30 +2,18 @@ import React from "react";
 import moment from "moment";
 import { Link } from "react-router-dom";
 
+import StoryTitleDomainLink from "./../StoryTitleDomainLink/StoryTitleDomainLink";
+
 const Story = ({ index, by, id, score, time, title, url, descendants }) => {
-  // The URLs get a little tricky.
-  let domain = "";
   let linkToComments = "";
   if (!url) {
     // TODO
-    // If the story comes from HN, there is no URL supplied. We need to create a request to get to the story
-    // Also the link to the comments should take the user to the actual story
+    // If the story comes from HN, the link to the comments should take the user to the actual story
     console.log(id);
   } else {
-    // Else we need to extract just the domain name to display
-    domain =
-      "(" +
-      url
-        .replace("http://", "")
-        .replace("https://", "")
-        .replace("www.", "")
-        .split(/[/?#]/)
-        .shift() +
-      ")";
-
     // Because we need to display a few data points about the story in the comments section,
     // let's create a var we can pass as state to the Link component
-    const storyInfo = { by, id, score, time, title, url, domain };
+    const storyInfo = { by, id, score, time, title, url };
     linkToComments = (
       <Link to={{ pathname: `/storycomments/${id}`, state: { ...storyInfo } }}>
         {descendants} comments
@@ -39,8 +27,8 @@ const Story = ({ index, by, id, score, time, title, url, descendants }) => {
   return (
     <div className="Story">
       <div>
-        <span>{index}. &#9650;</span> <a href={url}>{title}</a>
-        <span className="Story__domain">{domain}</span>
+        <span>{index}. &#9650;</span>{" "}
+        <StoryTitleDomainLink url={url} title={title} />
       </div>
       <div className="Story__lower">
         <span>
---
+++ src/components/UI/StoryTitleDomainLink/StoryTitleDomainLink.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+// Component used to display the story title, domain it's from, and create links to both
+// eg. Early Riser or Night Owl? New Study Helps to Explain the Difference (directorsblog.nih.gov)
+const StoryTitleDomainLink = ({ url, title }) => {
+  // The URLs get a little tricky.
+  let domain = "";
+  if (!url) {
+    // TODO
+    // If the story comes from HN, there is no URL supplied. We need to create a request to get to the story
+  } else {
+    // Else we need to extract just the domain name to display
+    domain =
+      "(" +
+      url
+        .replace("http://", "")
+        .replace("https://", "")
+        .replace("www.", "")
+        .split(/[/?#]/)
+        .shift() +
+      ")";
+  }
+
+  // TODO
+  // The domain link below needs to preform a query for all Hn stories related to that domain
+  return (
+    <>
+      <a href={url}>{title}</a>
+
+      <span className="Story__domain"> {domain}</span>
+    </>
+  );
+};
+
+export default StoryTitleDomainLink;
